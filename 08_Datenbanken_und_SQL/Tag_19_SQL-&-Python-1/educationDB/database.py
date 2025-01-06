cd228dfe3a0e173d323dc9448a0f1f6cb739bc99
HyBRiZx420 <mitch.hermanos@web.de>
2025-01-06 19:24:14 +0100

    update
    ...

--- 08_Datenbanken_und_SQL/Tag_19_SQL-&-Python-1/educationDB/database.py
+++ 08_Datenbanken_und_SQL/Tag_19_SQL-&-Python-1/educationDB/database.py
@@ -1,8 +1,19 @@
+from datetime import datetime
 import sqlite3
 import pandas as pd
 
-# Erstellt eine Datenbank, sofern nicht vorhanden. Stellt sonst einfach Verbindung her:
-connection = sqlite3.connect(r'/Users/Admin/Documents/1_augustkurs/08_SQL/databases/lernen.db')
+##############
+# CONNECTION #
+##############
+
+connection = sqlite3.connect(
+	r"C:\Users\Admin\OneDrive\Dokumente\DataCraft\DataAnalyst_Aug.24\08_Datenbanken_und_SQL\databases\lernen.db")
+
+################
+# CREATE TABLE #
+################
+
+# create_table Query:
 
 create_table_sql = '''
     CREATE TABLE IF NOT EXISTS gelerntes (
@@ -11,18 +22,48 @@ create_table_sql = '''
     );
 '''
 
+
+#############
+# FUNCTIONS #
+#############
+
+# fn create_table:
+
 def create_table():
-    with connection:
-        connection.execute(create_table_sql)
+	with connection:
+		connection.execute(create_table_sql)
+
+
+# fn insert_topic:
 
 def insert_topic():
-    """Fragt den Nutzer nach einem Eintrag, den er hinzufügen will.
-    und fügt diesen dann zur Tabelle gelerntes hinzu.
-    """
-    pass
+	"""Fragt den Nutzer nach einem Eintrag, den er hinzufügen will.
+	und fügt diesen dann zur Tabelle gelerntes hinzu.
+	"""
+	thema = input(
+		"Was hast du gelernt? (bitte Thema eintragen!): \n\n")  # Eingabeaufforderung (Thema)
+	datum = datetime.now()  # erstellt Timestamp (Datum)
+	with connection:
+		connection.execute("INSERT INTO gelerntes (Datum, Thema) VALUES (?, ?)",
+						   (datum, thema))  # insertion-Query
+		connection.commit()  # commit um die Einträge zu speichern
+		print(f"Thema hinzugefügt: {thema} am {datum.strftime('%Y-%m-%d %H:%M:%S')}")
+
+
+# fn view_entries:
 
 def view_entries():
-    """Zeigt dem Nutzer alle (alternativ auch die obersten zehn o.Ä.)
-    Einträge in der Tabelle gelerntes an.
-    """
-    pass
+	"""Zeigt dem Nutzer alle (alternativ auch die obersten zehn o.Ä.)
+	Einträge in der Tabelle gelerntes an.
+	"""
+	with connection:
+		cursor = connection.cursor()  # erstellt cursor
+		cursor.execute(
+			"SELECT * FROM gelerntes ORDER BY Datum DESC")  # cursor selection-Query, lädt cursor auf
+		entries = cursor.fetchall()  # alle daten abrufen
+	if entries:  # wenn es Einträge gibt...
+		for entry in entries:  # für Eintrag in Einträge
+			datum, thema = entry  # datum, thema ist der Eintrag
+			print(f"{datum}: {thema} \n")
+	else:
+		print("keine Einträge vorhanden.")
